373998f6356cc42cdc19e1d93183f2117d9793b1
Mr. Solo Dev <knightwor496@gmail.com>
2023-12-16 19:30:22 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,28 @@
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import UplaodImage from "./components/UplaodImage";
+import Editor from "./components/Editor";
 
 function App() {
+  
+  const [visiblity, setVisiblity] = useState(true)
+  const [editorVisiblity, setEditorVisiblity] = useState(false)
+  const [imagePath, setImagePath] = useState('')
+
   return (
     <div className="App">
+
+      <UplaodImage visible={visiblity} saveFile={(event)=>{
+        
+        if (event.target.files && event.target.files[0]) {
+          setImagePath(URL.createObjectURL(event.target.files[0]));
+        }
+
+        setVisiblity(false)
+        setEditorVisiblity(true)
+      }}/>
+
+      <Editor visible={editorVisiblity} path={imagePath}/>
       
     </div>
   );
